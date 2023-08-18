d762a886e05c7b535b63a164577b79a964a71a98
Mahdi Lamb <mahdilamb@gmail.com>
2023-08-19 00:40:03 +0100

    Update code-quality-and-tests.yaml (#4)
    * Update code-quality-and-tests.yaml
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>

--- pydantic_stream/__init__.py
+++ pydantic_stream/__init__.py
@@ -1,5 +1,5 @@
 """Main entry point for Pydantic Streamer."""
 from pydantic_stream.model import resolve, stream_model
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __all__ = ["stream_model", "resolve"]
